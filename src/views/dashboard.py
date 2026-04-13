3cde78072d8548ffbb8c6866c5da9e56a6f5fb57
YeseniaMirelesMendez <yeseniamireles215@gmail.com>
2026-04-13 17:11:42 -0600

    tarea

---
+++ src/views/dashboard.py
@@ -0,0 +1,21 @@
+import flet as ft
+
+def Dashboardview(page, tarea_controller):
+    user = page.session.get("user")
+    lista_tareas = ft.Column(scroll=ft.ScrollMode.ALWAYS, expand=True)
+    
+    def refresh():
+    lista_tareas.controls.clear()
+    for t in tarea_controller.obtener_lista(user['id_usuario']):
+        lista_tareas.controls.append(
+            ft.Card(
+                content=ft.Container(
+                    content=ft-ListTile(
+                        title=ft.Text(f"{t['description']}\nPrioridad: {t['´prioridad']}"),
+                        subtitle=ft.Badge(f"{t['description']}\nPrioridad: {t['prioridad']}"),
+                        trailing=ft.Badge(content=ft.Text(t['estado']), bgcolor=ft.Colors.ORANGE_300)
+                    ), padding=10
+                )
+            )
+        )
+        page.update()
